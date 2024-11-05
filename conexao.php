c83c1712cee8a4cbac34bc185958f9925022388e
Isabelle Pacheco <isapachecosenai@gmail.com>
2024-11-05 15:52:49 -0300

    Initial commit

---
+++ conexao.php
@@ -0,0 +1,17 @@
+<?php
+
+    $servername = "localhost:3309";
+    $username ="root";
+    $password ="";
+    $dbname ="crudS";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+
+if ($conn -> connect_error){
+
+    echo "Erro conexão" . $conn -> connect_error;
+
+}
+
+?>
